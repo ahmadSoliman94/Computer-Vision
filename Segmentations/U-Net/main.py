b00722fbf66c3729f169702a2e36dce154fb75fb
root <ahmad.suliman94@hotmail.com>
2023-05-27 18:34:11 +0000

    Unet

--- Segmentations/U-Net/main.py
+++ Segmentations/U-Net/main.py
@@ -1,7 +1,16 @@
-import tensorflow as tf
 from UNet_model import UNet 
-import Read_preprocess_data as rp
 import os
+import tensorflow as tf
+import os
+import random
+import numpy as np
+ 
+from tqdm import tqdm 
+
+from skimage.io import imread, imshow
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+
 
 
 # main function
@@ -10,59 +19,107 @@ import os
 
 if __name__ == "__main__":
 
-    # read and preprocess the data
-    # Set the parameters
+
     seed = 42
-    # IMG_WIDTH = 256
-    # IMG_HEIGHT = 256
+    np.random.seed = seed
+
+    IMG_WIDTH = 128
+    IMG_HEIGHT = 128
     IMG_CHANNELS = 3
+
     TRAIN_PATH = './dataset/train/'
     TEST_PATH = './dataset/test/'
 
-    # Set the seed
-    rp.set_seed(seed)
+    train_ids = next(os.walk(TRAIN_PATH))[1]
+    test_ids = next(os.walk(TEST_PATH))[1]
 
-    # Read and preprocess the training data
-    train_ids = next(iter(os.walk(TRAIN_PATH)))[1]
-    X_train, Y_train = rp.read_train_images(train_ids, TRAIN_PATH, channels=IMG_CHANNELS)
+    X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool_)
 
-    print("X_train shape: {}".format(len(X_train)))
-    print("Y_train shape: {}".format(len(Y_train)))
+    print('Resizing training images and masks')
+    for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):   
+        path = TRAIN_PATH + id_
+        img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]  
+        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_train[n] = img  #Fill empty X_train with values from img
+        mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool_)
+        for mask_file in next(os.walk(path + '/masks/'))[2]:
+            mask_ = imread(path + '/masks/' + mask_file)
+            mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',  
+                                          preserve_range=True), axis=-1)
+            mask = np.maximum(mask, mask_)  
 
-    # Read and preprocess the test data
-    test_ids = next(iter(os.walk(TEST_PATH)))[1]
-    X_test = rp.read_test_images(test_ids, TEST_PATH, channels= IMG_CHANNELS)
+        Y_train[n] = mask   
 
-    print("X_test shape: {}".format(len(X_test)))
+    # test images
+    X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    sizes_test = []
+    print('Resizing test images') 
+    for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+        path = TEST_PATH + id_
+        img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+        sizes_test.append([img.shape[0], img.shape[1]])
+        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_test[n] = img
 
     print('Done!')
 
-    # Visualize the data
-    # visualize_data(X_train, Y_train)
+    image_x = random.randint(0, len(train_ids))
+    imshow(X_train[image_x])
+    plt.show()
+    imshow(np.squeeze(Y_train[image_x]))
+    plt.show()
+
 
 
 
       
-    # # create the model
-    # IMG_WIDTH = 256
-    # IMG_HEIGHT = 256
-    # IMG_CHANNELS = 3
+    # create the model
+    unet = UNet(input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), num_classes=1)  # create the model
 
-    # unet = UNet(input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), num_classes=1)  # create the model
+    model = unet.build()  # build the model
+    print(model.summary())  # print the model summary
 
-    # model = unet.build()  # build the model
-    # print(model.summary())  # print the model summary
+    # # save the model as a png file
+    # tf.keras.utils.plot_model(model, to_file='./U-Net.png', show_shapes=True, show_layer_names=True)  # save the model as a png file
 
-    # # # save the model as a png file
-    # # tf.keras.utils.plot_model(model, to_file='./U-Net.png', show_shapes=True, show_layer_names=True)  # save the model as a png file
+    #  compile the model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    # #  compile the model
-    # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    # #  fit the model
-    # model.fit(X_train, Y_train, validation_split=0.1, epochs=50, batch_size=16)
-
+    #  fit the model
+    model.fit(X_train, Y_train, validation_split=0.1, epochs=50, batch_size=16)
     
+    
+    
+        ####################################
+
+    idx = random.randint(0, len(X_train))
 
 
+    preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+    preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+    preds_test = model.predict(X_test, verbose=1)
 
+
+    preds_train_t = (preds_train > 0.5).astype(np.uint8)
+    preds_val_t = (preds_val > 0.5).astype(np.uint8)
+    preds_test_t = (preds_test > 0.5).astype(np.uint8)
+
+
+    # Perform a sanity check on some random training samples
+    ix = random.randint(0, len(preds_train_t))
+    imshow(X_train[ix])
+    plt.show()
+    imshow(np.squeeze(Y_train[ix]))
+    plt.show()
+    imshow(np.squeeze(preds_train_t[ix]))
+    plt.show()
+
+    # Perform a sanity check on some random validation samples
+    ix = random.randint(0, len(preds_val_t))
+    imshow(X_train[int(X_train.shape[0]*0.9):][ix])
+    plt.show()
+    imshow(np.squeeze(Y_train[int(Y_train.shape[0]*0.9):][ix]))
+    plt.show()
+    imshow(np.squeeze(preds_val_t[ix]))
+    plt.show()
